7036e10abfd19d80ca37ed2e7ad2920f8181c2c2
Sam Perumal <sam_lki@yahoo.com>
2020-02-04 07:58:03 +0000

    Additional permutation for JHB

--- py/city_sites.py
+++ py/city_sites.py
@@ -73,7 +73,7 @@ def parseJhb():
         element1 = browser.find_element_by_id("MSOZoneCell_WebPartWPQ3")
         element2 = element1.find_element_by_class_name("ms-rtestate-field")
         text = element2.get_attribute('innerText')
-        match = re.search(r"stage\W+(\d+)\W+(in-progress|is\W+in\W+progress|will\W+be\W+implemented)", text, re.IGNORECASE)
+        match = re.search(r"stage\W+(\d+)\W+-?\W*(in-?\W*progress|is\W+in\W+progress|will\W+be\W+implemented)", text, re.IGNORECASE)
         
         if match != None: return (match.group(1), match.group(0))
         else: return (None, None)
