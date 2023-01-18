b87b9ffcbd83aa2aa96e4e685cc35a7be77c5a65
Sam Perumal <sam_lki@yahoo.com>
2023-01-18 15:19:22 +0200

    Formatting fixes

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,8 @@ const state = reactive<State>({
   schedule: null
 })
 
+type DisplaySchedule = { active: Boolean, text: string, date: string }
+
 function loadSchedule() {
   fetch("https://cptloadshed.blob.core.windows.net/stage/current.json", {
     headers: { "x-metaplex": "loadshed" }
@@ -69,31 +71,46 @@ onMounted(() => {
 })
 
 const days = computed<DateTime[]>(() => {
-  const offsets = [0, 1, 2, 3, 4]
+  const offsets = [0, 1, 2, 3, 4, 10]
   const dates = offsets.map(ofst => DateTime.now().plus({ days: ofst }))
 
   return dates
 })
 
-const currentStage = computed<{ show: Boolean, text: string }>(() => {
+const latestSchedule = computed<Map<string, DisplaySchedule[]>>(() => {
   let ret = { show: false, text: "" }
+  let schedDict = new Map<string, DisplaySchedule[]>()
+
   if (state.schedule != null && state.schedule.stages != null) {
     const now = DateTime.now()
-    for (const stage of state.schedule.stages) {
+
+    let schedule = state.schedule.stages.map(stage => {
       const start = DateTime.fromISO(stage.start)
       const end = DateTime.fromISO(stage.end)
-      if (start <= now && now <= end) {
-        ret.show = true
-        ret.text = `Stage ${stage.stage} from ${start.toLocaleString(DateTime.TIME_24_SIMPLE)} until ${end.toLocaleString(DateTime.TIME_24_SIMPLE)}`
+
+      return {
+        text: `Stage ${stage.stage} from ${start.toLocaleString(DateTime.TIME_24_SIMPLE)} until ${end.toLocaleString(DateTime.TIME_24_SIMPLE)}`,
+        date: start.setLocale('en-ZA').toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY),
+        end: end,
+        active: (start <= now && now <= end)
       }
+    })
+      .filter(sched => sched.end > DateTime.now())
+
+
+    for (const sched of schedule) {
+      if (!schedDict.has(sched.date))
+        schedDict.set(sched.date, []);
+
+      (schedDict.get(sched.date) ?? []).push(sched)
     }
   }
 
-  return ret;
+  return schedDict;
 })
 
 function mapBlock(value: number, index: number, day: number, state: State): Block {
-  let desc = "Possible"
+  let desc = ""
   const ret = {
     stage: value,
     desc: desc,
@@ -101,19 +118,21 @@ function mapBlock(value: number, index: number, day: number, state: State): Bloc
     show: value > 0 && value <= state.stage
   }
 
+  const today = DateTime.now().day
+
   if (state.schedule != null) {
-    const start = DateTime.now().startOf('day').plus({ hours: index * 2 })
-    const end = DateTime.now().startOf('day').plus({ hours: (index + 1) * 2 })
-  
+    const start = DateTime.now().startOf('day').plus({ hours: index * 2, days: day - today })
+    const end = DateTime.now().startOf('day').plus({ hours: (index + 1) * 2, days: day - today })
+
     for (const period of state.schedule.stages) {
       const periodStart = DateTime.fromISO(period.start)
       const periodEnd = DateTime.fromISO(period.end)
-      
+
       if (0 < value && value <= period.stage && periodStart < end && periodEnd >= start) {
-        ret.desc = "Confirmed"
+        ret.desc = " - Confirmed"
         ret.show = true
       }
-      
+
     }
   }
 
@@ -129,13 +148,16 @@ const dayData = computed(() => {
       weekday: 'short'
     }),
     date: d,
-    slots: stageData[state.zone][d.day - 1].map((v, i) => mapBlock(v, i, d.day, state))
+    slots: stageData[state.zone][d.day - 1]
+      .map((v, i) => mapBlock(v, i, d.day, state))
+      .filter(b => b.show)
   }))
 })
 </script>
 
 <template>
-  <header style="display: flex; flex-direction: column; place-content: center;">
+  <h3>Cape Town Loadshedding</h3>
+  <header>
     <div>Zone:
       <select v-model="state.zone">
         <option v-for="zone in zones" :value="zone">{{ zone }}</option>
@@ -146,48 +168,73 @@ const dayData = computed(() => {
         <option v-for="stage in stages" :value="stage">{{ stage }}</option>
       </select>
     </div>
-    <div v-if="state.schedule != null">
-      <div>Updated at {{ state.schedule.time }} from <a :href="state.schedule.url">{{ state.schedule.site }}</a></div>
-      <div>{{ currentStage.text }}</div>
-    </div>
   </header>
 
   <main>
-    <div v-for="day in dayData">
-      <div>{{ day.disp }}</div>
-      <div v-if="day.slots != null && day.slots.length == 0">No load shedding</div>
+    <div v-for="day in dayData" class="day">
+      <div class="date">{{ day.disp }}</div>
+      <div v-if="day.slots != null && day.slots.length == 0" class="no-shed">No load shedding</div>
       <div v-for="(slot, index) in day.slots">
-        <div v-if="slot.show">{{ slot.period }} [Stage {{ slot.stage }} - {{ slot.desc }}]</div>
+        <div v-if="slot.show">{{ slot.period }} [Stage {{ slot.stage }}{{ slot.desc }}]</div>
       </div>
     </div>
   </main>
+
+  <footer>
+
+    <div v-if="state.schedule != null">
+      <div style="margin-bottom: 0.5rem;"><a :href="state.schedule.url">{{ state.schedule.site }}</a> schedule updated
+        at {{ state.schedule.time }}</div>
+      <div v-for="schedPair in latestSchedule" class="sched-day">
+        <div>{{ schedPair[0] }}</div>
+        <div v-for="sched in schedPair[1]">{{ sched.text }}</div>
+      </div>
+    </div>
+
+    <div class="developer">
+      <div>Developed by Sameshan Perumal</div>
+      <div>
+        <a href="https://datacartographer.com">https://datacartographer.com</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-top: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.day {
+  margin: 1rem 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.day .date {
+  text-align: center;
+  border-bottom: 1px solid gray;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.day .no-shed {
+  text-align: center;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+footer {
+  border-top: 1px solid gray;
+  margin-top: 2rem;
+  text-align: center;
+}
+
+.sched-day {
+  margin-bottom: 1rem;
+}
+
+.developer {
+  border-top: 1px solid gray;
+  margin-top: 2rem;
 }
 </style>
